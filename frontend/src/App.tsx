04b3c31fcb7b44adb0123dbe4df441b85d32e42e
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-25 15:18:27 +0200

    connectionComplete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Todo = {
+    id: string;
+    description: string;
+    status: string;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        axios.get("/api/todo")
+            .then(response => setTodos(response.data))
+            .catch(error => console.log(error));
+    }, []);
+
+
+    return (
+        <>
+            <h1>ToDo Liste</h1>
+            <ul>
+                {todos.map((todo)=> (
+                    <li key={todo.id}>
+                        {todo.description} - {todo.status}
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
-export default App
+
