b866ff6854b18ec5f74f217519cfe6668e5e2d6e
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-25 15:42:46 +0200

    Frontend entry

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,24 +10,53 @@ type Todo = {
 
 export default function App() {
     const [todos, setTodos] = useState<Todo[]>([]);
+    const [description, setDescription] = useState<string>("");
 
+    // get Todos
     useEffect(() => {
         axios.get("/api/todo")
             .then(response => setTodos(response.data))
             .catch(error => console.log(error));
+
+        axios.post("api/todo",)
     }, []);
+
+    // add Todos
+    function addTodo() {
+        const newTodo: Partial<Todo> = {
+            description: description,
+            status: "OPEN"
+        };
+
+        // post
+        axios.post("api/todo", newTodo)
+            .then(response => {
+                setTodos([...todos, response.data]);
+                setDescription("");
+            })
+            .catch(error => console.log(error));
+    }
 
 
     return (
         <>
             <h1>ToDo Liste</h1>
             <ul>
-                {todos.map((todo)=> (
+                {todos.map((todo) => (
                     <li key={todo.id}>
                         {todo.description} - {todo.status}
                     </li>
                 ))}
             </ul>
+
+            <h2>Neues ToDo hinzufügen</h2>
+            <input
+                type={"text"}
+                value={description}
+                onChange={event => setDescription(event.target.value)}
+                placeholder={"Todo eingeben"}
+            />
+            <button onClick={addTodo}>Hinzufügen</button>
         </>
     )
 }
