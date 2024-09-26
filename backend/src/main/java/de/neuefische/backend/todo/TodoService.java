d2b16b30094715caaf36e0867a01f27a40929619
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-26 13:54:38 +0200

    done

--- backend/src/main/java/de/neuefische/backend/todo/TodoService.java
+++ backend/src/main/java/de/neuefische/backend/todo/TodoService.java
@@ -1,50 +1,9 @@
-//package de.neuefische.backend.todo;
-//
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Service
-//class TodoService {
-//
-//    private final TodoRepository todoRepository;
-//
-//    TodoService(TodoRepository todoRepository) {
-//        this.todoRepository = todoRepository;
-//    }
-//
-//    List<Todo> getAll() {
-//        return todoRepository.getAll();
-//    }
-//
-//    public Todo save(Todo todo) {
-//        String id = UUID.randomUUID().toString();
-//
-//        Todo todoToSave = todo.withId(id);
-//
-//        return todoRepository.save(todoToSave);
-//    }
-//
-//    public Todo getById(String id) {
-//        return todoRepository.getById(id);
-//    }
-//
-//    public Todo update(Todo todo) {
-//        return todoRepository.update(todo);
-//    }
-//
-//    public void delete(String id) {
-//        todoRepository.delete(id);
-//    }
-//}
-
 package de.neuefische.backend.todo;
 
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
+
 
 @Service
 class TodoService {
@@ -56,23 +15,23 @@ class TodoService {
     }
 
     List<Todo> getAll() {
-        return todoRepository.findAll(); // Nutze die Methode von MongoRepository
+        return todoRepository.findAll();
     }
 
     public Todo save(Todo todo) {
-        return todoRepository.save(todo); // MongoDB verwaltet IDs selbst
+        return todoRepository.save(todo);
     }
 
     public Todo getById(String id) {
-        return todoRepository.findById(id).orElse(null); // Überprüfe auf Optional
+        return todoRepository.findById(id).orElse(null);
     }
 
     public Todo update(Todo todo) {
-        return todoRepository.save(todo); // Update mit save, wenn das Objekt existiert
+        return todoRepository.save(todo);
     }
 
     public void delete(String id) {
-        todoRepository.deleteById(id); // Nutze die Methode von MongoRepository
+        todoRepository.deleteById(id);
     }
 }
 
